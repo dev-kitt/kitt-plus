7d4dda68751c58f1eefb98f5605dbc06883519b9
8Kit <kit@made.llc>
2024-04-28 17:55:13 -0400

    env

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -4,6 +4,8 @@ part 'env.g.dart';
 
 @Envied(path: '.env')
 abstract class Env {
+  @EnviedField(varName: 'GOOGLEAI_API_KEY', obfuscate: true)
+  static final String googleAiApiKey = _Env.googleAiApiKey;
   @EnviedField(varName: 'OPENAI_API_KEY', obfuscate: true)
-  static final openAiApiKey = _Env.openAiApiKey;
+  static final String openAiApiKey = _Env.openAiApiKey;
 }
